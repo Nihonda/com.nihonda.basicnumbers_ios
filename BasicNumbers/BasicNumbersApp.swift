b5fc0843c50620d8c75700547d55212c447b3a3e
Nurlan Kalybekov <carjapancar@gmail.com>
2024-08-17 09:06:50 +0900

    Initial Commit

---
+++ BasicNumbers/BasicNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicNumbersApp.swift
+//  BasicNumbers
+//
+//  Created by Nurlan on 2024/08/17.
+//
+
+import SwiftUI
+
+@main
+struct BasicNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
